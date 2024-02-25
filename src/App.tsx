66d65b9eebd38b3676a9c3a0d9d7197994f7f4a7
Akash BG <akashbg4@gmail.com>
2024-02-25 11:21:18 +0530

    intial setup for bubblegram

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
-import { Route, Routes } from "react-router-dom"
-
 const App = () => {
     return (
-        <main className="flex h-screen">
-            <Routes>
-                <Route path="/sign-in" element={<SignInForm />}>
-
-                </Route>
-                <Route index element={<Home />}>
-
-                </Route>
-            </Routes>
-        </main>
+        <div>App</div>
     )
 }
 
